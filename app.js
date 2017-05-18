e781a04cad5877d5e89fcd47c094974fbc006e05
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2017-05-18 12:01:29 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.MathJax = {
+    jax: ["input/TeX", "output/CommonHTML"],
+    extensions: ["tex2jax.js"],
+    displayAlign: "left",
+    showMathMenu: false,
+    TeX: {
+        extensions: ["AMSmath.js"]
+    }
+};
